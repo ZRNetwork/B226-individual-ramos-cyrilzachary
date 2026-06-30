c1cfda202bd8c43c477e5bd5fe1984037223c1e5
Zachary <zchry.rms@gmail.com>
2026-06-30 19:28:12 +0800

    Activity

--- src/day11/activity/Activity7_Ramos.java
+++ src/day11/activity/Activity7_Ramos.java
@@ -1,15 +1,16 @@
+package day11.activity;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package day10.activity;
 
 /**
  *
  * @author ZR
  */
-public class Activity6_Ramos {
+public class Activity7_Ramos {
     public static void main(String[] args) {
         
     }
---
+++ src/day11/discussion/SingleDimensionalArray.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day11.discussion;
+
+/**
+ *
+ * @author ZR
+ */
+public class SingleDimensionalArray {
+    public static void main(String[] args) {
+         int[] ages = {24, 43, 35, 18, 28, 25};
+         System.out.println(ages[2]);
+         
+         String[] names = {"Michael", "Leslie", "Kent", "Ray", "Jeff"};
+         System.out.println(names.length);
+         
+    }
+}
