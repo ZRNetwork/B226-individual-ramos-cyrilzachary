f731d1db67c604c65e623827c03fe27401e54a00
Zachary <zchry.rms@gmail.com>
2026-07-01 19:37:53 +0800

    Activity 8

---
+++ src/day12/discussion/MultidimensionalArray.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day12.discussion;
+
+/**
+ *
+ * @author ZR
+ */
+public class MultidimensionalArray {
+    public static void main(String[] args) {
+        // Syntax
+        
+        // Single
+        String[] color = {};
+        //Multiple
+        String[][] colors = {{"White", "Yellow", "Pnk"},{"Black", "Brown", "Gray"}};
+    }
+}
